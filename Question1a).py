621e1349f34e2faf42a8da6fad870c1740a3fbe3
Shae Williams <shaewilliams@live.com>
2026-06-02 12:21:05 +0200

    Update Question1a).py

--- Question1a).py
+++ Question1a).py
@@ -6,10 +6,16 @@ Sigma1=0.2
 Sigma2=0.3
 rho=0.3
 T=1
+K=100
 
 def CorrBrownian(R): #R is the amount of replications
     Z=np.random.normal(loc=0,scale=np.sqrt(T),size=(2,R)) #Could've used standard normal, but this is more general
     L=np.array([[1,0],[rho,np.sqrt(1-rho**2)]])
     CorrZ=np.matmul(L,Z)
     return CorrZ
+def BasketValues(CorrZ):
+    S_1=S_0*np.exp((r-0.5*Sigma1**2)*T+Sigma1*CorrZ[0])
+    S_2=S_0*np.exp((r-0.5*Sigma2**2)*T+Sigma2*CorrZ[1])
+    return np.exp(-r*T)*np.maximum(0.5*S_1+0.5*S_2-K,0)
+
 
